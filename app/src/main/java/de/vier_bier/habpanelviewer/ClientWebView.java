868def5a0650aae05a1d2632a61263c13bae34e0
Volker Bier <volker.bier@web.de>
2018-10-23 14:58:43 +0200

    work around bug where first press is ignored when devices with immersive mode is disabled.
    Signed-off-by: Volker Bier <volker.bier@web.de>

--- app/src/main/java/de/vier_bier/habpanelviewer/ClientWebView.java
+++ app/src/main/java/de/vier_bier/habpanelviewer/ClientWebView.java
@@ -71,14 +71,14 @@ public class ClientWebView extends WebView implements NetworkTracker.INetworkLis
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
+
         if (hasFocus) {
-            setSystemUiVisibility(
+            postDelayed(() -> setSystemUiVisibility(
                     (mImmersive ? View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY : 0));
-
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY : 0)), 500);
         }
     }
 
