0f1431e2082b2c865ba23ecabb32cd842283fa8d
vbier <volker.bier@web.de>
2020-06-24 15:32:50 +0200

    fix pause on screen dim.
    Signed-off-by: vbier <volker.bier@web.de>

--- app/src/main/java/de/vier_bier/habpanelviewer/MainActivity.java
+++ app/src/main/java/de/vier_bier/habpanelviewer/MainActivity.java
@@ -295,7 +295,11 @@ public class MainActivity extends ScreenControllingActivity
             mCommandQueue.addHandler(new InternalCommandHandler(this, mMotionDetector, mServerConnection));
             mCommandQueue.addHandler(new AdminHandler(this));
             mCommandQueue.addHandler(new BluetoothHandler(this, (BluetoothManager) getSystemService(BLUETOOTH_SERVICE)));
-            mCommandQueue.addHandler(new ScreenHandler((PowerManager) getSystemService(POWER_SERVICE), this, () -> mWebView.post(() -> mWebView.pause())));
+            mCommandQueue.addHandler(new ScreenHandler((PowerManager) getSystemService(POWER_SERVICE), this, () -> {
+                if (prefs.getBoolean(Constants.PREF_PAUSE_WEBVIEW, false)) {
+                    mWebView.post(() -> mWebView.pause());
+                }
+            }));
             mCommandQueue.addHandler(new VolumeHandler(this, (AudioManager) getSystemService(Context.AUDIO_SERVICE)));
             mCommandQueue.addHandler(new NotificationHandler(this));
             mCommandQueue.addHandler(new TtsHandler(this));
